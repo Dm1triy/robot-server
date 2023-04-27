28c2091c6f0a2c788a496204024143c0bcfabb60
Dm1triy <Dmitriy.nn06@gmail.com>
2023-04-27 14:22:45 +0300

    server & client

--- kuka-client.py
+++ kuka-client.py
@@ -1,3 +1,13 @@
 import socket
 
-host = 'kpu-RAY-S131-Mi'
+host = '192.168.88.24'
+
+port = 6543  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
+
--- server.py
+++ server.py
@@ -1,17 +1,54 @@
 import socket
+from accel_listener import Accel
+import threading as thr
 
-host = socket.gethostname()
-print(host)
-port = 6543
 
-with socket.socket() as s:
-    s.bind((host, port))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+class Server:
+    def __init__(self, host='192.168.88.24', port=6543):
+        self.accel_stream = Accel()
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket()
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+        print('Port opened')
+
+        self.server_is_running = True
+
+        conn, address = self.server_socket.accept()
+        self.conn = conn
+        self.conn_addres = address
+        print("Connection from: " + str(address))
+
+    def __del__(self):
+        self.conn.close()
+
+    def interaction(self):
+        while self.server_is_running:
+            data = self.conn.recv(1024).decode()
+            if data == "Give me acceleration":
+                self.send_data()
+                continue
+            if data == "Kill":
+                self.server_is_running = False
+
+    def send_data(self):
+        data = self.accel_stream.get_data()
+        while not data:
+            data = self.accel_stream.get_data()
+        self.conn.sendall(data)
+
+
+
+# with socket.socket() as s:
+#     s.bind((host, port))
+#     s.listen()
+#     conn, addr = s.accept()
+#     with conn:
+#         print(f"Connected by {addr}")
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#             conn.sendall(data)
+
